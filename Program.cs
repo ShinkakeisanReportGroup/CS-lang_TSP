85fe4072fe80e668364edce1842cb67e264574da
kai-maker <kai1010@outlook.jp>
2018-12-05 23:08:52 +0900

    [add] initalize

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS_lang_TSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
